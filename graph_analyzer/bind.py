15adfb3ed34c6baaa211b53613864e366a929b0f
TAJh <2559659915@qq.com>
2024-09-03 10:42:22 +0800

    add bind code info

---
+++ graph_analyzer/bind.py
@@ -0,0 +1,43 @@
+import os
+import csv
+from typing import Dict, List
+from pathlib import Path
+from graph_analyzer.graph import GraphNode
+
+
+def bind_code_info_for_data(input_dir: str, nodes: List[GraphNode]) -> Dict[str, str]:    
+    npy_files = find_npy_files(input_dir)
+    match_dict = {}
+    for node in nodes:
+        # 屏蔽子图节点
+        if node.is_subgraph:
+            continue
+        scope_name = node.scope.replace("/", "_")
+        code_info = '\n'.join(node.code_info)
+        match_dict[scope_name] = code_info
+    
+    bind_result = {}
+    for npy_file in npy_files:
+        npy_path = os.path.realpath(npy_file)
+        basename = os.path.basename(npy_path)
+        node_scope = basename.split(".")[0]
+        bind_code = match_dict.get(node_scope, None)
+        bind_result[npy_path] = bind_code
+    return bind_result
+
+
+def find_npy_files(directory):
+    npy_files = list(Path(directory).rglob('*.npy'))
+    return npy_files
+
+
+def write_to_csv(param: Dict, file_name: str = 'output.csv'):
+    # 打开CSV文件以写入模式
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # 遍历字典并逐行写入键值对
+        for key, value in param.items():
+            writer.writerow([key, value])
+
+        
--- graph_analyzer/main.py
+++ graph_analyzer/main.py
@@ -3,13 +3,15 @@ import sys
 import argparse
 from typing import List
 from graph_analyzer.graph_parser import Parser
-
+from graph_analyzer.bind import bind_code_info_for_data
 
 
 
 def main():
     parser = argparse.ArgumentParser(description="IR Parser")
     parser.add_argument('--input', type=str, required=True, help="Path to the input file")
+    parser.add_argument('--data', type=str, required=False, default="./", help="Path to data dir")
+    parser.add_argument('--output', type=str, required=False, default="./", help="Path to data dir")
     args = parser.parse_args()
 
     file_path = args.input
@@ -20,38 +22,39 @@ def main():
     parser.parse(input_text)
 
     nodes = parser.get_nodes()
+    bind_result = bind_code_info_for_data(arg.data, nodes)
+    write_to_csv(bind_result, os.path.join(args.output, "mapping.csv"))
+    # func_graph_output = []
+    # node_output = []
 
-    func_graph_output = []
-    node_output = []
-
-    func_graph_output.append("Graph Info:")
-    for node_name, info in nodes.items():
-        if info.is_subgraph:
-            node_list = [ii.name for ii in info.nodes.values()]
-            func_graph_output.append(f"Subgraph: {info.name}\nAttributes: {info.attrs}\nSubNodes: {node_list}")
-        else:
-            code_info = '\n'.join(info.code_info)
-            node_output.append(f"Variable: {info.name}\nOperator: {info.operator_name}\nInputs: {info.var_inputs}\nScope: {info.scope}\nCode Info: {code_info}\n")
-        sorted_nodes = []
-        visited = {var_name: False for var_name in nodes.keys()}
-        info.topological_sort(sorted_nodes, visited, parser)
+    # func_graph_output.append("Graph Info:")
+    # for node_name, info in nodes.items():
+    #     if info.is_subgraph:
+    #         node_list = [ii.name for ii in info.nodes.values()]
+    #         func_graph_output.append(f"Subgraph: {info.name}\nAttributes: {info.attrs}\nSubNodes: {node_list}")
+    #     else:
+    #         code_info = '\n'.join(info.code_info)
+    #         node_output.append(f"Variable: {info.name}\nOperator: {info.operator_name}\nInputs: {info.predecessors}\nScope: {info.scope}\nCode Info: {code_info}\n")
+    #     sorted_nodes = []
+    #     visited = {var_name: False for var_name in nodes.keys()}
+    #     info.topological_sort(sorted_nodes, visited, parser)
         
-        # Example: Ancestors trace back
-        ancestors = []
-        visited = {var_name: False for var_name in nodes.keys()}
-        info.trace_back_ancestors(ancestors, visited, parser)
-    total_nodes, total_cnodes = parser.count_nodes()
-    func_graph_output.append(f"\nNode counting information:")
-    func_graph_output.append(f"Total number of nodes: {total_nodes}")
-    func_graph_output.append(f"Total number of cnodes: {total_cnodes}")
+    #     # Example: Ancestors trace back
+    #     ancestors = []
+    #     visited = {var_name: False for var_name in nodes.keys()}
+    #     info.trace_back_ancestors(ancestors, visited, parser)
+    # total_nodes, total_cnodes = parser.count_nodes()
+    # func_graph_output.append(f"\nNode counting information:")
+    # func_graph_output.append(f"Total number of nodes: {total_nodes}")
+    # func_graph_output.append(f"Total number of cnodes: {total_cnodes}")
 
-    output = "\n".join(func_graph_output + node_output)
-    print(output)
+    # output = "\n".join(func_graph_output + node_output)
+    # print(output)
     
-    while(True):
-        cmd = input("please write your command: ")
-        if cmd == "stop":
-            break
+    # while(True):
+    #     cmd = input("please write your command: ")
+    #     if cmd == "stop":
+    #         break
 
 if __name__ == "__main__":
     main()
